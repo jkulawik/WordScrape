1ad3ce1e33fdc98f4b544b152ea8c9a340f08982
jacob <jacob@samochud.home>
2023-10-10 18:23:24 +0200

    refactor main into separate files

---
+++ getwebtext.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func getWebsiteText(sourceURL string) string {
+	response, err := http.Get(sourceURL)
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		warningLogger.Print(response.Status, " -- skipping website: ", sourceURL)
+	}
+
+	contentType := response.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "text/html") {
+		warningLogger.Print("Unexpected website content type", " -- skipping website: ", sourceURL)
+	}
+
+	tokenizer := html.NewTokenizer(response.Body)
+	previousTokenStartsScript := false
+	var fullText string
+
+	for {
+		tokenType := tokenizer.Next()
+
+		if tokenType == html.ErrorToken {
+			err = tokenizer.Err()
+			if err == io.EOF {
+				break
+			} else {
+				errorLogger.Print(err)
+				break
+			}
+		} else if tokenType == html.StartTagToken {
+			token := tokenizer.Token()
+			previousTokenStartsScript = token.Data == "script"
+		} else if tokenType == html.TextToken && !previousTokenStartsScript {
+			token := tokenizer.Token()
+			fullText += token.Data
+		}
+	}
+	return fullText
+}
--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"strings"
-
-	"golang.org/x/net/html"
 )
 
 var (
@@ -35,52 +30,12 @@ func main() {
 	URL := "https://quotes.toscrape.com/page/2/"
 	// URL = "https://www.moddb.com/news/an-unfortunate-delay-yet-plenty-of-good-news"
 
-	response, err := http.Get(URL)
-	if err != nil {
-		errorLogger.Fatal(err)
-	}
-
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		warningLogger.Print(response.Status, " -- skipping website: ", URL)
-	}
-
-	contentType := response.Header.Get("Content-Type")
-	if !strings.HasPrefix(contentType, "text/html") {
-		warningLogger.Print("Unexpected website content type", " -- skipping website: ", URL)
-	}
-
-	tokenizer := html.NewTokenizer(response.Body)
-	previousTokenStartsScript := false
-	var fullText string
-
-	for {
-
-		tokenType := tokenizer.Next()
-
-		if tokenType == html.ErrorToken {
-			err = tokenizer.Err()
-			if err == io.EOF {
-				break
-			} else {
-				errorLogger.Print(err)
-				break
-			}
-		} else if tokenType == html.StartTagToken {
-			token := tokenizer.Token()
-			previousTokenStartsScript = token.Data == "script"
-		} else if tokenType == html.TextToken && !previousTokenStartsScript {
-			token := tokenizer.Token()
-			fullText += token.Data
-		}
-	}
-
+	fullText := getWebsiteText(URL)
 	words := getWords(fullText)
 	// fmt.Print(words)
 	// fmt.Printf("%q", text) // adds quotes around each element
 
-	err = writeWordCache(URL, words)
+	err := writeWordCache(URL, words)
 	if err != nil {
 		warningLogger.Print(err)
 	}
