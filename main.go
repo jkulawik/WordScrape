1808410e27fd913fb4ce6979f963625f4d3c6ee8
jacob <jacob@samochud.home>
2023-10-12 17:26:50 +0200

    fix a race condition

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func getWordsFromURL(sourceURL string) []string {
 	infoLogger.Print("Scraping ", sourceURL)
 	fullText, err := getWebsiteText(sourceURL)
 	if err != nil {
-		errorLogger.Print(sourceURL, err, " -- website words will be skipped")
+		errorLogger.Print(sourceURL, " ", err, " -- website will be skipped")
 		return nil
 	}
 
@@ -55,7 +55,8 @@ func getWordsFromURL(sourceURL string) []string {
 func getWordsFromURLWorker(jobs chan string, results chan []string, waitGroup *sync.WaitGroup) {
 	// Consume jobs
 	for url := range jobs {
-		results <- getWordsFromURL(url)
+		words := getWordsFromURL(url)
+		results <- words
 	}
 	waitGroup.Done()
 }
@@ -65,6 +66,8 @@ func main() {
 	infoLogger.Print("Starting WordScrape")
 
 	URLs := []string{
+		// "https://quotes.toscrape.com/doesntexist/",
+		// "https://www.moddb.com/news/doesntexist/",
 		"https://quotes.toscrape.com/page/2/",
 		"https://quotes.toscrape.com/page/3/",
 		"https://quotes.toscrape.com/page/4/",
@@ -72,8 +75,8 @@ func main() {
 		// "https://www.moddb.com/news/an-unfortunate-delay-yet-plenty-of-good-news",
 	}
 
-	workerCount := 2
 	var allWords []string
+	workerCount := 2
 	jobs := make(chan string)
 	results := make(chan []string)
 
@@ -86,10 +89,15 @@ func main() {
 	}()
 
 	// Start consuming results
+	var resultsWaitGroup sync.WaitGroup
+	resultsWaitGroup.Add(1)
 	go func() {
 		for result := range results {
 			allWords = append(allWords, result...)
 		}
+		// must wait for the last append to finish;
+		// without it there was a race condition where the last result was sometimes not being included in the stats
+		resultsWaitGroup.Done()
 	}()
 
 	// Start workers
@@ -102,6 +110,7 @@ func main() {
 	// Wait for all jobs to be consumed
 	workerWaitGroup.Wait()
 	close(results)
+	resultsWaitGroup.Wait()
 
 	stats := getTopFrequentWords(allWords, 5)
 	fmt.Println("\nResults:")
